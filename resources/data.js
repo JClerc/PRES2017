d9751bad81a7a620453add1e56fa1a6b31197639
JClerc <jices25@gmail.com>
2017-02-07 20:14:55 +0100

    Init commit

---
+++ resources/data.js
@@ -0,0 +1,160 @@
+window.jsonp_response({
+  "themes": {
+
+    "defense_securite": {
+      "label": "Défense & sécurité",
+      "steps": []
+    },
+    "depenses_publiques": {
+      "label": "Dépenses publiques",
+      "steps": []
+    },
+    "reforme_etat": {
+      "label": "Réforme de l'Etat",
+      "steps": []
+    },
+    "taxes_impots": {
+      "label": "Taxes & impôts",
+      "steps": []
+    },
+    "education_culture": {
+      "label": "Éducation & culture",
+      "steps": []
+    },
+    "dialogue_social": {
+      "label": "Dialogue social",
+      "steps": []
+    },
+    "sante": {
+      "label": "Santé",
+      "steps": []
+    },
+    "transparence": {
+      "label": "Transparence",
+      "steps": []
+    },
+    "environnement_ecologie": {
+      "label": "Environnement & écologie",
+      "steps": [
+        {
+          "candidates": {
+            "macron": {
+              "label": "Emmanuel Macron",
+              "img": "img/candidates/macron.png",
+              "program": {
+                "title": "Regroupons les services de l’Etat",
+                "content": [
+                  "« Je veux placer la nouvelle écologie au coeur de la politique qui sera menée en France au cours des prochaines années et au coeur des politiques que développera l’Union européenne »"
+                ]
+              }
+            },
+            "dupont": {
+              "label": "Nicolas Dupont-Aignan",
+              "img": "img/candidates/dupont.png",
+              "program": {
+                "title": "0% d’énergie fossile",
+                "content": [
+                  "Prolonger le nucléaire de 40 à 60 ans (et renforcer les études de réacteurs de 4ème genération.",
+                  "Autoriser l’auto-consommation d’energie produite de manière autonome."
+                ]
+              }
+            },
+            "fillon": {
+              "label": "François Fillon",
+              "img": "img/candidates/fillon.png",
+              "program": {
+                "title": "La voiture et l’environnement",
+                "content": [
+                  "Faire les aménagements routiers nécessaires pour faire sauter les principaux bouchons (moins de bouchons égal moins de pollution) Créer des parkings supplémentaires (gratuits pour les titulaires du Pass Navigo) près des stations de RER."
+                ]
+              }
+            }
+          }
+        },
+        {
+          "candidates": {
+            "lepen": {
+              "label": "Marine Le Pen",
+              "img": "img/candidates/lepen.png",
+              "program": {
+                "title": "Garantir la sécurité alimentaire",
+                "content": [
+                  "Relocaliser la production alimentaire en France et mettre en avant la protection animale.",
+                  "Soutenir la recherche sur les energies renouvelables. Maintenir l’énergie nucléaire à moyen terme."
+                ]
+              }
+            },
+            "melenchon": {
+              "label": "Jean-Luc Mélenchon",
+              "img": "img/candidates/melenchon.png",
+              "program": {
+                "title": "Sortir du nucléaire",
+                "content": [
+                  "Sortir du nucléaire pour des raisons de sécurité nationale.",
+                  "Il faut passer sur au 100% renouvelable et également aller vers une alimentation plus végétale."
+                ]
+              }
+            },
+            "poutou": {
+              "label": "Philippe Poutou",
+              "img": "img/candidates/poutou.png",
+              "program": {
+                "title": "Émissions excessives de gaz..",
+                "content": [
+                  "Baisse rapide du recours aux énergies fossiles (pétrole, charbon, gaz). Développer massivement les transports en commun gratuits, d’entamer la reconversion industrielle des secteurs les plus polluants : pétrochimie vers le renouvelable, ..."
+                ]
+              }
+            }
+          }
+        },
+        {
+          "candidates": {
+            "hamon": {
+              "label": "Benoît Hamon",
+              "img": "img/candidates/hamon.png",
+              "program": {
+                "title": "Plan zéro déchets",
+                "content": [
+                  "Sortie du diesel d’ici 2025, et interdiction des pesticides dangeureux.",
+                  "Investir dans le bio et l’agroécologie et lancement d’un plan contre la maltraitance agricole."
+                ]
+              }
+            },
+            "lassalle": {
+              "label": "Jean Lassalle",
+              "img": "img/candidates/lassalle.png",
+              "program": {
+                "title": "Responsabilité de l’écologie",
+                "content": [
+                  "Retirer la responsabilité de l’écologie et de la gestion des biens communs et la donner aux communes et communautés locales."
+                ]
+              }
+            },
+            "arthaud": {
+              "label": "Nathalie Arthaud",
+              "img": "img/candidates/arthaud.png",
+              "program": {
+                "title": "Technologies dangereuses",
+                "content": [
+                  "Dénoncer les pratiques du système capitaliste, en indiquant que les technologies (OGMs, Nucléaire.. ) sont dangereuses car elles sont au mains de la bourgeoisie. Le danger serait moindre si ces technologies etaient sous le contrôle du peuple."
+                ]
+              }
+            }
+          }
+        }
+      ]
+    },
+    "europe_international": {
+      "label": "Europe et international",
+      "steps": []
+    },
+    "fonction_publique": {
+      "label": "Fonction publique",
+      "steps": []
+    },
+    "social": {
+      "label": "Social",
+      "steps": []
+    }
+  }
+});
